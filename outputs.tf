8900f9ee9b71797cdc929d72ad93a9620e668142
Arun Sanna <arunchowdary.sanna@cms.hhs.gov>
2022-05-02 15:36:38 -0700

    updated code and linted

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output zone_data {
-    value = data.aws_route53_zone.cms_zone
+output "zone_data" {
+  value = data.aws_route53_zone.cms_zone
 }
 
-output gitlab_dns {
-    value =  aws_route53_record.routes.*.fqdn
+output "gitlab_dns" {
+  value = aws_route53_record.routes.*.fqdn
 }
--- variables.tf
+++ variables.tf
@@ -1,28 +1,18 @@
-variable elb_dns {
-    description = "NLB DNS endpoint"
-    default = ""
+variable "elb_dns" {
+  description = "NLB DNS endpoint"
+  default     = ""
 }
 
-variable zone_dns {
-    description = "wild card dns for each account"
-    default = ""
-}
-
-variable environment {
-    description = "environment"
-    default = "dev"
-}
-
-variable cluster_name {
-    description = "this will create records in dev without conficts"
-    default = "srii-dev"
+variable "zone_dns" {
+  description = "wild card dns for each account"
+  default     = ""
 }
 
 variable "apps" {
-    default = [
-        "kiali.batcave-dev.internal.cms.gov",
-        "code.batcave-dev.internal.cms.gov",
-        "tracing.batcave-dev.internal.cms.gov",
-        "grafana.batcave-dev.internal.cms.gov"
-    ]
+  default = [
+    "kiali.batcave-dev.internal.cms.gov",
+    "code.batcave-dev.internal.cms.gov",
+    "tracing.batcave-dev.internal.cms.gov",
+    "grafana.batcave-dev.internal.cms.gov"
+  ]
 }
