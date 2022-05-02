4f39fe4ed7fe14f42ff699bc60d670a24aae90a0
Arun Sanna <arunchowdary.sanna@cms.hhs.gov>
2022-05-02 12:51:52 -0700

    typos fix

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ data "aws_route53_zone" "cms_zone" {
 
 # Route53 record for kiali service
 resource "aws_route53_record" "kiali" {
-  zone_id = aws_route53_zone.cms_zone.id
-  name    = "kiali-${cluster-name}.batcave-dev.internal.cms.gov"
+  zone_id = data.aws_route53_zone.cms_zone.id
+  name    = "kiali-${var.cluster-name}.batcave-dev.internal.cms.gov"
   type    = "CNAME"
   ttl     = "60"
   records = [var.elb_dns]
