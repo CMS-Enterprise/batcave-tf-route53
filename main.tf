45114bc8db51a0c81a981ed9fd664e33dddd1a66
Arun Sanna <arunchowdary.sanna@cms.hhs.gov>
2022-05-02 12:48:09 -0700

    updated code

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 # Extract existing data from AWS
 data "aws_route53_zone" "cms_zone" {
-  name         = "batcave-dev.internal.cms.gov"
+  name         = var.zone_dns
   private_zone = true
 }
 
 # Route53 record for kiali service
-# resource "aws_route53_record" "kiali" {
-#   zone_id = aws_route53_zone.primary.zone_id
-#   name    = "kiali.batcave-dev.internal.cms.gov"
-#   type    = "CNAME"
-#   ttl     = "60"
-#   records = [var.elb_dns]
-# }
+resource "aws_route53_record" "kiali" {
+  zone_id = aws_route53_zone.cms_zone.id
+  name    = "kiali-${cluster-name}.batcave-dev.internal.cms.gov
+  type    = "CNAME"
+  ttl     = "60"
+  records = [var.elb_dns]
+}
+
--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable elb_dns {
     description = "NLB DNS endpoint"
     default = ""
 }
+
+variable zone_dns {
+    description = "wild card dns for each account"
+    default = ""
+}
+
+variable environment {
+    description = "environment"
+    default = "dev"
+}
+
+variable cluster_name {
+    description = "this will create records in dev without conficts"
+    default = "srii-dev"
+}
