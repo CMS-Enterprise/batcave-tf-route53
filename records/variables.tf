3c85a683b265fd698129dc2fd7513a4aa27f44c0
Charles Bushong <charles.bushong@cms.hhs.gov>
2023-12-21 14:40:32 -0500

    Fix records submodule

--- records/variables.tf
+++ records/variables.tf
@@ -1,5 +1,23 @@
-variable "hosted_zone_id" {}
-variable "hosted_zone_dns" {}
-variable "subdomains" {}
-variable "ttl" {}
-variable "endpoint" {}
+variable "hosted_zone_id" {
+  description = "The ID of the hosted zone to create records in."
+  type        = string
+}
+
+
+variable "hosted_zone_dns" {
+  description = "The DNS name of the hosted zone to create records in."
+  type        = string
+}
+
+variable "subdomains" {
+  description = "A list of subdomains to create records for."
+  type        = list(string)
+}
+variable "ttl" {
+  description = "The TTL of the records to create."
+  type        = number
+}
+variable "endpoint" {
+  description = "The endpoint to create records for."
+  type        = string
+}
